365cf34e049d26d1663cc62deb9593e508e1a432
mayur <mayur@lenovo>
2022-10-02 23:49:44 +0530

    First commit

---
+++ Day-1_Git and GitHub/src/org/tnsidia/basic/EclipseWithGithub.java
@@ -0,0 +1,10 @@
+package org.tnsidia.basic;
+
+public class EclipseWithGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Java Program");
+	}
+
+}
