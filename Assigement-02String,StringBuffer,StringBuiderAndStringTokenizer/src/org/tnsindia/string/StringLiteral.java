2cefc9a827c5c761763f4c80fd90b5ac9dfcaeb2
mayur <mayur@lenovo>
2022-10-11 12:08:42 +0530

    first commit

---
+++ Assigement-02String,StringBuffer,StringBuiderAndStringTokenizer/src/org/tnsindia/string/StringLiteral.java
@@ -0,0 +1,16 @@
+package org.tnsindia.string;
+
+import java.util.Scanner;
+
+public class StringLiteral {
+	public static void main(String[] args) {
+		//String str="mayur ahire";
+		//System.out.println(str);
+		Scanner s=new Scanner(System.in);
+		String str1=s.nextLine();
+		System.out.println(str1);
+		s.close();
+
+	}
+
+}
